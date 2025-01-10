7b538c08520e27dc7cec2bb696445d2ffbaf9f5b
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-01-09 20:21:18 -0400

    bucles resuelto

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,12 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  let producto = 1;
+  for (let i = a; i <= b; i++) {
+    producto *= i;
+    console.log(producto)
+  }
+  return producto || 0;
 }
-
+console.log(productoEntreNúmeros(-5,5))
 module.exports = productoEntreNúmeros;
