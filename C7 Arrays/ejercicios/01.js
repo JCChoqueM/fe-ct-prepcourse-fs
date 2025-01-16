cee41eab01bc3873af42dc4112a25307f4fcde3f
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-01-16 01:49:39 -0400

    pueba

--- C7 Arrays/ejercicios/01.js
+++ C7 Arrays/ejercicios/01.js
@@ -2,6 +2,7 @@ function devolverPrimerElemento(array) {
   // Retornar el primer elemento del arreglo recibido.
   // Tu código:
   return array[0];
+  console.log(message)
 }
 
 module.exports = devolverPrimerElemento;
